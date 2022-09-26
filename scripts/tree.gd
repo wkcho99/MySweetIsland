5d787e978fc199a90e0cff43d193c344aea4afd4
Oskar Moberg <oskar.e.moberg@gmail.com>
2022-09-26 16:44:35 +0200

    Something

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -5,13 +5,10 @@ extends RigidBody
 # var a = 2
 # var b = "text"
 onready var _animator := $AnimationPlayer
-<<<<<<< HEAD
 var fall = false
 var can_cut = false
-=======
 onready var _particles := $Particles
 
->>>>>>> 61e9a940cdc51e64f4b99f587932b99f11d19cd4
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var world = $World
@@ -25,19 +22,11 @@ func _process(delta):
 	_fall()
 
 func _fall():
-<<<<<<< HEAD
 	if get_node("../Player/PlayerBody").hit && fall == false && can_cut: 
 		_animator.play("fall")
 		fall = true
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-=======
-	if get_node("../Player/PlayerBody").hit : 
-		_animator.play("fall")
 		_particles.emitting = false
 
 
 func _sparkle():
 	pass
->>>>>>> 61e9a940cdc51e64f4b99f587932b99f11d19cd4
